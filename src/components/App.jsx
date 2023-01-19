a28d8b9b4a7057e32b532fe6ca3b5ec2abd7f60a
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2023-01-19 16:05:13 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+import  ContactsList  from './ContactsList/ContactsList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Wrap } from './App.styled';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+
+
+
+export default function App() {
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrap>
+      <Section title={`Phonebook`}></Section>
+      <ContactForm />
+      <Section title={`Contacts`}>
+        <Filter />
+        {isLoading && !error && <b>Loading...</b>}
+        <ContactsList />
+      </Section>
+    </Wrap>
   );
-};
+}
